969d3a2506a48a5c83c053916ca5cf7548442652
Elliot Blanchard <public@elliotblanchard.com>
2020-04-07 00:37:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,21 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Top column
+  [1,4,7], # Middle column
+  [2,5,8], # Bottom column
+  [0,4,8], # Diag A
+  [6,4,2], # Diag B
+]
+
+def won?(board)
+  WIN_COMBINATIONS.any? do |win_state|
+    if win_state.all? {|index| position_taken?(board,index)}
+      return win_state
+    end
+  end
+end
